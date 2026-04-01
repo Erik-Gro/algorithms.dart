bcc2c6a2d6f594240df5b9476e7754130e69ec3c
Erik <147524279+Erik-Gro@users.noreply.github.com>
2026-04-01 14:24:57 +0000

    cleannup and improve readability

--- decompress.dart
+++ decompress.dart
@@ -8,43 +8,24 @@ void main() {
 
 String decompress(String string) {
   String result = "";
-  print(string.length);
+
   for (int i = 0; i < string.length; i++) {
-    // if (i + 1 == string.length) {
-    //   result += string[i];
-    //   continue;
-    // }
-    String stringRepetitions = '';
     String char = string[i];
-    // int right = i +1;
-    print(i);
-    while (i < string.length - 1 && int.tryParse(string[i + 1]) != null) {
+    String stringRepetitions = '';
+
+    while (i + 1 < string.length && int.tryParse(string[i + 1]) != null) {
       stringRepetitions += string[i + 1];
       i++;
-      // print(i);
-      // right++;
     }
 
-    int numberstringRepetitions = 0;
-
-    if (stringRepetitions != '') {
-      numberstringRepetitions = int.parse(stringRepetitions);
-    }
-    // i += repetitions.length -1;
-    if (numberstringRepetitions != 0) {
-      // int counter = int.parse(string[i + 1]);
-      for (int j = 0; j < numberstringRepetitions; j++) {
+    if (stringRepetitions.isNotEmpty) {
+      int count = int.parse(stringRepetitions);
+      for (int j = 0; j < count; j++) {
         result += char;
       }
-      // i++;
       continue;
     }
-    if (i +1 == string.length) {
-      result += string[i];
-      continue;
-    }
-    result += string[i];
-    // i++;
+    result += char;
   }
   return result;
 }
